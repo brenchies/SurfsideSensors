7a1527bc9da11866253ed08d58df79df8391ac9d
Manuel Rojas <m.i.rojas.franco@student.tue.nl>
2022-08-08 21:53:21 +0200

    surfscidescience library with template and stdarg

--- src/WaterQuality/WaterQuality/src/main.cpp
+++ src/WaterQuality/WaterQuality/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <surfsidescience.h>
+#include <tinygsmwrapper.h>
+
+surfSideScience myscience("WATER_QUALITY_01");
+TinyGSMWrapper mysim("www.google.com", "/", "web.digicelaruba.com", "","", 115200, 25, 27, 26, 4, 80);
 
 void setup() {
   // put your setup code here, to run once:
+  myscience.uploadData<TinyGSMWrapper>(mysim);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
 }
