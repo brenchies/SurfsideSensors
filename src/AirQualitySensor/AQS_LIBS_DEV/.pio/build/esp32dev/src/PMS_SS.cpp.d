86bf12a70f65fa851aff98bea18092f3bcc70c8d
Sean-David <460282@student.fontys.nl>
2022-08-12 20:25:21 +0200

    Adding AQS files here

---
+++ src/AirQualitySensor/AQS_LIBS_DEV/.pio/build/esp32dev/src/PMS_SS.cpp.d
@@ -0,0 +1,7 @@
+.pio/build/esp32dev/src/PMS_SS.cpp.o: src/PMS_SS.cpp include/PMS_SS.h \
+ .pio/libdeps/esp32dev/PMS\ Library/src/PMS.h \
+ C:/Users/seand/.platformio/packages/framework-arduinoespressif32/cores/esp32/Stream.h \
+ C:/Users/seand/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
+ C:/Users/seand/.platformio/packages/framework-arduinoespressif32/cores/esp32/WString.h \
+ C:/Users/seand/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
+ C:/Users/seand/.platformio/packages/framework-arduinoespressif32/cores/esp32/Printable.h
